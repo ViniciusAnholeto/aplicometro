c64cb8a94fe3da83f1de9d762fc2a3599a893448
Vincius Anholeto <vini-anholeto@hotmail.com>
2025-06-06 18:04:01 -0300

    Initialize Spring Boot application with Maven setup and basic configurations

---
+++ src/main/java/com/viniciusanholeto/aplicometro/AplicometroApplication.java
@@ -0,0 +1,13 @@
+package com.viniciusanholeto.aplicometro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AplicometroApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AplicometroApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/viniciusanholeto/aplicometro/AplicometroApplicationTests.java
@@ -0,0 +1,13 @@
+package com.viniciusanholeto.aplicometro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AplicometroApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
